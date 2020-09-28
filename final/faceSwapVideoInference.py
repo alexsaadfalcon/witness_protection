fea1cf7351f5723a812d51fc6e07e1b22c834ee0
alexsaadfalcon <alexsaadfalcon@yahoo.com>
2020-09-27 23:44:11 -0400

    Update faceSwapVideoInference.py

--- final/faceSwapVideoInference.py
+++ final/faceSwapVideoInference.py
@@ -164,7 +164,7 @@ def warpTriangle(img1, img2, t1, t2):
 
 
 # if __name__ == '__main__':
-def swapVideo(imagename='alregib.jpg', videoname='input.mp4', outname='output.mp4'):
+def swapVideo(imagename='pratt.jpg', videoname='input1.mp4', outname='output.mp4'):
     # Make sure OpenCV is version 3.0 or above
     (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
@@ -310,7 +310,6 @@ def swapVideo(imagename='alregib.jpg', videoname='input.mp4', outname='output.mp
     cap.release()
     cv2.destroyAllWindows()
 
-#swapVideo(imagename='alregib', videoname='input.mp4')
 
 imagenames = ['evans.jpg', 'hemsworth.jpg', 'pratt.jpg']
 for i in range(3):
